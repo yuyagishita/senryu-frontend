acec88f3f99a565d000b7ae7fb481111e21bb9fd
yu-yagishita <yu.yagishita1995@gmail.com>
2020-04-15 09:40:00 +0900

    love-mentorリポジトリから移行

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Store from './store';
+import App from './App';
+import register from './serviceWorker';
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
+
+register();
