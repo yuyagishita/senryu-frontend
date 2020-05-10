57819bf581f43ab4fc9d5106238d6315d44efa0c
yu-yagishita <yu.yagishita1995@gmail.com>
2020-05-10 16:20:35 +0900

    asset&meta&css学習

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,20 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
-    return (
-    <>
-    <h1>First Post</h1>
-    <h2>
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
+      <h1>First Post</h1>
+      <h2>
         <Link href="/">
-            <a>Back to home</a>
+          <a>Back to home</a>
         </Link>
-    </h2>
-    </>
-    )
+      </h2>
+    </Layout>
+  );
 }
