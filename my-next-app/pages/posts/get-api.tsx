2ce422ffaf312d25ac49861f24effb934dcc2ad6
yu-yagishita <yu.yagishita1995@gmail.com>
2020-05-14 21:31:56 +0900

    fetchでAPI連携中

---
+++ my-next-app/pages/posts/get-api.tsx
@@ -0,0 +1,53 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+
+export default function Post({
+  postData,
+}: {
+  postData: {
+    user: { firstName: string; lastName: string; username: string };
+  };
+}) {
+  return (
+    <Layout>
+      <Head></Head>
+      {postData.user.firstName}
+      {postData.user.lastName}
+      {postData.user.username}
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const url = "http://localhost:8080/login";
+  //   const method = "POST";
+  //   const body = { username: "yagiyu", password: "miran" };
+  //   const headers = {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   };
+  const res = await fetch(url, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ username: "yagiyu", password: "miran" }),
+  })
+    .then(function (response) {
+      const postData = response.json();
+    })
+    .then(function (myJson) {
+      console.log(JSON.stringify(myJson));
+    });
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
