bfb7904d7bdaacfcd18ab6f0f12c7da975654be7
yu-yagishita <yu.yagishita1995@gmail.com>
2020-05-26 23:45:28 +0900

    クライアント側にAPIの結果を返した

--- my-next-app/pages/api/login.ts
+++ my-next-app/pages/api/login.ts
@@ -24,5 +24,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   });
   const postData = await response.json();
   console.log(postData);
-  res.status(200).json({ text: "Hello" });
+  res.status(200).json({ postData });
 };
