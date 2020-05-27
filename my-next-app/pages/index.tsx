c26dfab76d191e24f25a13a32ef6a10806f0ec3b
yu-yagishita <yu.yagishita1995@gmail.com>
2020-05-27 22:25:02 +0900

    TOPからの画面遷移を作成

--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -12,6 +12,16 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <h1>NANPA TOP</h1>
+      <div>
+        <Link href="/login">
+          <a>ログイン</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/register">
+          <a>登録する</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
---
+++ my-next-app/pages/register.tsx
@@ -0,0 +1,78 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
+import Router from "next/router";
+import { emitKeypressEvents } from "readline";
+import { useCallback, useEffect, useState, HtmlHTMLAttributes } from "react";
+import fetch from "isomorphic-unfetch";
+import { userInfo, type } from "os";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  username: string;
+  password: string;
+};
+
+type PostData = {
+  error: string;
+};
+
+export default function Login() {
+  const { register, handleSubmit, watch, errors } = useForm<FormData>();
+  const onSubmit = useCallback(async (data: FormData) => {
+    console.log(data);
+    const url = "api/login";
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        username: data.username,
+        password: data.password,
+      }),
+    });
+    const postData: PostData = await response.json();
+    console.log(postData);
+    if (response.status == 200) {
+      Router.push("/auth");
+    } else {
+      alert(postData.error);
+    }
+  }, []);
+
+  console.log(watch("username"));
+  console.log(watch("password"));
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <h1>NANPA Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="filed">
+          <label className="label">username</label>
+          <input
+            name="username"
+            placeholder="username"
+            ref={register({ required: true })}
+          />
+          {errors.username && "usernameを入力してください。"}
+        </div>
+        <div className="filed">
+          <label className="label">password</label>
+          <input
+            name="password"
+            placeholder="password"
+            ref={register({ required: true })}
+          />
+          {errors.password && "passwordを入力してください。"}
+        </div>
+        <div className="filed">
+          <button>login</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
