f9510bbbeda2c83ab49faf24674f43336a27796b
yu-yagishita <yu.yagishita1995@gmail.com>
2020-06-01 22:25:10 +0900

    登録画面の内容変更

--- my-next-app/pages/auth.tsx
+++ my-next-app/pages/auth.tsx
@@ -1,9 +1,3 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
-import Date from "../components/date";
-import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
 export default function Login() {
@@ -11,11 +5,8 @@ export default function Login() {
   console.log(router.query);
 
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+    <>
       <h1>NANPA Auth</h1>
-    </Layout>
+    </>
   );
 }
--- my-next-app/pages/register.tsx
+++ my-next-app/pages/register.tsx
@@ -1,19 +1,8 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
-import Date from "../components/date";
-import { GetStaticProps } from "next";
-import Router from "next/router";
-import { emitKeypressEvents } from "readline";
-import { useCallback, useEffect, useState, HtmlHTMLAttributes } from "react";
-import fetch from "isomorphic-unfetch";
-import { userInfo, type } from "os";
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 
 type FormData = {
-  firstName: string;
-  lastName: string;
   username: string;
   email: string;
   password: string;
@@ -27,54 +16,16 @@ export default function Register() {
   const { register, handleSubmit, watch, errors } = useForm<FormData>();
   const onSubmit = useCallback(async (data: FormData) => {
     console.log(data);
-    // const url = "api/login";
-    // const response = await fetch(url, {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     username: data.username,
-    //     password: data.password,
-    //   }),
-    // });
-    // const postData: PostData = await response.json();
-    // console.log(postData);
-    // if (response.status == 200) {
-    //   Router.push("/auth");
-    // } else {
-    //   alert(postData.error);
-    // }
   }, []);
 
-  console.log(watch("firstName"));
-  console.log(watch("lastName"));
   console.log(watch("username"));
   console.log(watch("email"));
   console.log(watch("password"));
 
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+    <>
       <h1>NANPA Register</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="filed">
-          <label className="label">firstName</label>
-          <input
-            name="firstName"
-            placeholder="firstName"
-            ref={register({ required: true })}
-          />
-          {errors.firstName && "firstNameを入力してください。"}
-        </div>
-        <div className="filed">
-          <label className="label">lastName</label>
-          <input
-            name="lastName"
-            placeholder="lastName"
-            ref={register({ required: true })}
-          />
-          {errors.lastName && "lastNameを入力してください。"}
-        </div>
         <div className="filed">
           <label className="label">username</label>
           <input
@@ -106,6 +57,6 @@ export default function Register() {
           <button>登録する</button>
         </div>
       </form>
-    </Layout>
+    </>
   );
 }
