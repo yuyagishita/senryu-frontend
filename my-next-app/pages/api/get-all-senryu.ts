89d231df3dfa52964b0efdb613fd5c6ed8706863
yu-yagishita <yu.yagishita1995@gmail.com>
2020-07-08 22:07:08 +0900

    川柳の全データを表示できるようにしたい

---
+++ my-next-app/pages/api/get-all-senryu.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fetch from "isomorphic-unfetch";
+
+type GetAllResponseData = {
+  post: {
+    id: string;
+    kamigo: string;
+    nakashichi: string;
+    shimogo: string;
+    user_id: string;
+    signup_at: string;
+  };
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = "http://post:8080/get-all";
+  // try {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json charset=utf-8",
+    },
+    body: JSON.stringify({}),
+  });
+  const getAllResponseData: GetAllResponseData = await response.json();
+  console.log(getAllResponseData);
+  res.status(200).json({ post: getAllResponseData.post });
+  // }
+  // catch (err) {
+  //   console.error("全川柳データ取得処理でエラーが発生", err);
+  //   res.status(500).json({ error: "API側でエラー発生" });
+  // }
+};
--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -1,6 +1,44 @@
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
 
-export default function Home() {
+type GetAllSuccessData = {
+  post: {
+    id: string;
+    kamigo: string;
+    nakashichi: string;
+    shimogo: string;
+    user_id: string;
+    signup_at: string;
+  };
+};
+
+type GetAllFailureData = {
+  error: string;
+};
+
+export default function Index() {
+  const fetcher = async (url: string) => {
+    console.log(url);
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({}),
+    });
+    // if (response.status == 200) {
+    const getAllSuccessData: GetAllSuccessData = await response.json();
+    console.log(getAllSuccessData);
+    return response.json();
+    // } else {
+    //   const getAllFailureData: GetAllFailureData = await response.json();
+    //   console.log(getAllFailureData);
+    //   return getAllFailureData;
+    // }
+  };
+
+  const { data, error } = useSWR("/api/get-all-senryu", fetcher);
+  console.log(data);
+  console.log(error);
+
   return (
     <>
       <h1>SENRYU TOP</h1>
@@ -14,14 +52,29 @@ export default function Home() {
           <a>登録する</a>
         </Link>
       </div>
+      <div></div>
     </>
   );
 }
-export async function getServerSideProps() {
-  const url = "api/getAll";
-  const response = await fetch(url);
 
-  return {
-    props: {}, // will be passed to the page component as props
-  };
-}
+// function AllSenryu() {
+//   const fetcher = (url: string) =>
+//     fetch(url)
+//       .then((res) => res.json)
+//       .catch((error) => error);
+//   const { data, error } = useSWR("/api/getAll", fetcher);
+// }
+
+// export async function getServerSideProps() {
+//   const url = "http://lacalhost:3000/api/getAll";
+//   const response = await fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify({}),
+//   });
+//   const getAllSuccessData: GetAllSuccessData = await response.json();
+//   console.log(getAllSuccessData);
+
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
