86e1dacdf46b78be7717d3fcac1712ba148372de
yu-yagishita <yu.yagishita1995@gmail.com>
2020-09-01 23:51:36 +0900

    Mypageに該当ユーザーのデータを表示できるようにした

---
+++ my-next-app/pages/api/get-senryu.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fetch from "isomorphic-unfetch";
+
+type GetResponseData = {
+  posts: [
+    {
+      id: string;
+      kamigo: string;
+      nakashichi: string;
+      shimogo: string;
+      user_id: string;
+      signup_at: string;
+    }
+  ];
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id;
+  const url = "http://post:8080/get/" + id;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json charset=utf-8",
+      },
+      body: JSON.stringify({}),
+    });
+    const getResponseData: GetResponseData = await response.json();
+    console.log(getResponseData);
+    res.status(200).json({ posts: getResponseData.posts });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
---
+++ my-next-app/pages/users/[id].tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+type GetResponseData = {
+  posts: [
+    {
+      id: string;
+      kamigo: string;
+      nakashichi: string;
+      shimogo: string;
+      user_id: string;
+      signup_at: string;
+    }
+  ];
+};
+
+export default function MyPage() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const fetcher = async (url: string) => {
+    console.log(url);
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({}),
+    });
+    const getResponseData: GetResponseData = await response.json();
+    return getResponseData;
+  };
+
+  const { data, error } = useSWR("/api/get-senryu?id=" + id, fetcher);
+
+  console.log(data);
+
+  console.log(error);
+
+  console.log(id);
+
+  if (typeof data === "undefined") {
+    return <div>川柳データ取得に失敗</div>;
+  } else {
+    const listItems = data.posts.map((post) => (
+      <div key={post.id}>
+        {post.kamigo} {post.nakashichi} {post.shimogo}
+      </div>
+    ));
+    return (
+      <>
+        <h1>SENRYU MyPage</h1>
+        {listItems}
+      </>
+    );
+  }
+}
